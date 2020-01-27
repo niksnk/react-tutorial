1d7f364020db86618fc8295a50d5e955ea39a1b9
Nikolas Abraham <nikolas@FTCMAC01.local>
2020-01-27 09:12:59 -0800

    Updates

---
+++ precache-manifest.7e14f5f6b818f286a6e765d64443bef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a08ee3782ecec2be2bcf604dfea7362",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "4ddafea8522b49c8a2a0",
+    "url": "/react-tutorial/static/css/main.49612428.chunk.css"
+  },
+  {
+    "revision": "499253bf897e9e731b26",
+    "url": "/react-tutorial/static/js/2.f390c8c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.f390c8c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ddafea8522b49c8a2a0",
+    "url": "/react-tutorial/static/js/main.0623c8ce.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
